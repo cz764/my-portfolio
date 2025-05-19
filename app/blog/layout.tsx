71728a2df67992bd91a8c70be81f76d8b592cd5f
Chen Zhu <chenzhu@Chens-MacBook-Pro-Moopy.local>
2025-05-19 11:21:20 -0400

    explore css modules

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,3 +1,5 @@
+import styles from "./styles.module.css";
+
 export default function BlogLayout({
   children,
 }: {
@@ -7,7 +9,9 @@ export default function BlogLayout({
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       {/* Layout structure */}
       <nav>{/* Navigation links */}</nav>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main
+        className={`flex flex-col gap-[32px] row-start-2 items-center sm:items-start ${styles.blog}`}
+      >
         {children}
       </main>
       <footer>{/* Footer content */}</footer>
